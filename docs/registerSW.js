09fa45f63faaa5d77ae30a7ab8f0a0bae972fdf0
johnlomov <johnlomov@users.noreply.github.com>
2026-04-29 21:05:09 +0300

    New build

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/your-repo-name/sw.js', { scope: '/your-repo-name/' })})}
